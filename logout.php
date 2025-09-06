b3f9116ff4584d4f284e233d88f10705a9418a3d
taher-dev <monmoyzx@gmail.com>
2025-09-06 20:39:40 +0600

    🐛 FIX: logout session

--- logout.php
+++ logout.php
@@ -1,4 +1,16 @@
 <?php
+// 1. Start the session to access it
+session_start();
+
+// 2. Unset all session variables
+session_unset();
+
+// 3. Destroy the session completely
 session_destroy();
+
+// 4. Redirect the user to the homepage
 header("location: index.php");
+
+// 5. Stop the script from running further
+exit();
 ?>
